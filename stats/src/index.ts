a72b53b463fcb2791e2776808b228a2d2bf1d804
Josebautista10 <cooldj1020@gmail.com>
2023-03-08 13:15:23 -0700

    create another report class to print out the results in html

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -3,6 +3,7 @@ import { CsvFileReader } from './CsvFileReader'
 import { ConsoleReport } from './reportTargets/ConsoleReport'
 import { WinsAnalysis } from './analyzers/WinsAnalysis'
 import { Summary } from './Summary'
+import { HtmlReport } from './reportTargets/HtmlReport'
 
 const csvFileReader = new CsvFileReader('football.csv')
 
@@ -10,6 +11,7 @@ const matchReader = new MatchReader(csvFileReader)
 matchReader.load()
 
 
-const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport())
+// const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport())
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport())
 
 summary.buildAndPrintReport(matchReader.matches)
