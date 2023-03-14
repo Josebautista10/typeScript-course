b2a27d62565cf2391caf3dd5e319de2f13537ba3
Josebautista10 <cooldj1020@gmail.com>
2023-03-13 23:54:48 -0600

    finish user model

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,19 +1,6 @@
+// all the users are getting pushed to db.json using json-server
 import { User } from './models/User'
-
-const user = new User({ name: 'John', age: 26 })
-
-user.set({ age: 55 })
-
-user.on('change', () => {
-  console.log('change 1')
-})
-user.on('change', () => {
-  console.log('change 2')
-})
-
-user.on('save', () => {
-  console.log('this is save')
-})
-
-user.trigger('save')
-console.log(user.events)
+// axios.get(`http://localhost:3000/users/${this.get('id')}`)
+const user = new User({ name: 'testing adding a user', age: 5 })
+user.save()
+console.log(user)
