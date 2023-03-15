b3ab0a49bc94f0139228ad333250a3da64098cd4
Josebautista10 <cooldj1020@gmail.com>
2023-03-14 21:46:59 -0600

    refactor code to be generic

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,6 @@
 // all the users are getting pushed to db.json using json-server
 import { User } from './models/User'
 // axios.get(`http://localhost:3000/users/${this.get('id')}`)
-const user = new User({ name: 'testing adding a user', age: 5 })
-user.save()
-console.log(user)
+// const user = new User({ name: 'testing adding a user', age: 5 })
+// user.save()
+// console.log(user)
